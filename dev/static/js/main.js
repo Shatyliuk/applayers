72883658f210f19beccf9a1efc0bcd6041c5aedd
Shatyliuk <shatyliuk@gmail.com>
2018-03-20 16:49:23 +0200

    add adaptive to index

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -2,5 +2,10 @@ $(document).ready(function () {
     $('.nav__item:has(ul)').on('click', function (e) {
         e.preventDefault();
         $('.nav__item .dropdown').slideToggle();
+    });
+
+    $('#navIcon').on('click', function () {
+        $(this).toggleClass('open');
+        $('#navbar').slideToggle();
     })
 });
