e80bfe61b61b9f2d17ce7e1bda46d003d31dfad6
Shatyliuk <shatyliuk@gmail.com>
2018-03-21 17:05:04 +0200

    add about us page

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -7,5 +7,23 @@ $(document).ready(function () {
     $('#navIcon').on('click', function () {
         $(this).toggleClass('open');
         $('#navbar').slideToggle();
-    })
+    });
+
+    //accordion
+
+    var acc = document.getElementsByClassName('accordion__heading');
+
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener('click', function () {
+            this.classList.toggle('active');
+
+            var panel = this.nextElementSibling;
+
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + 'px';
+            }
+        })
+    }
 });
